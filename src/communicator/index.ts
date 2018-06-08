df55a7b399f27535a11e14068ec33c4423d5a376
Chris Hopkins <chris@liquid-state.com>
2018-06-08 13:17:43 +1000

    Cleanup noop implementation of communicator, moved the iwa-desktop

--- src/communicator/index.ts
+++ src/communicator/index.ts
@@ -1,11 +1,9 @@
 import Communicator, { ICommunicatorImpl, INativeCommunicator } from './communicator';
 import createUrlCommunicator from './urlImpl';
-import createNoopCommunicator from './noopImpl';
 
 export {
   Communicator,
   createUrlCommunicator,
-  createNoopCommunicator,
   ICommunicatorImpl,
   INativeCommunicator as ICommunicator,
 };
