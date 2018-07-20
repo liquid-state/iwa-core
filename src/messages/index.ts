dd07d1b95855aab4e18e8dd22a804da677ec7679
Cyril Doussin <cyril@liquid-state.com>
2018-07-20 11:28:56 +1000

    Add set_ready message utility funciton.

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,9 +1,11 @@
 import * as config from './config';
 import * as launch from './launch';
+import * as iwa from './iwa';
 import * as kv from './kv';
 
 export default {
   config,
   launch,
+  iwa,
   kv,
 };
---
+++ src/messages/iwa.ts
@@ -0,0 +1,5 @@
+export const setReady = () => ({
+  domain: 'iwa',
+  eventType: 'set_ready',
+  data: {},
+});
