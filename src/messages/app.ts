766e68942281fdb9497946b64ec41d930dec5bd2
Cyril Doussin <cyril@liquid-state.com>
2018-07-26 15:39:17 +1000

    Add app/reset event type utility function

---
+++ src/messages/app.ts
@@ -0,0 +1,6 @@
+export const reset = () => ({
+    domain: 'app',
+    eventType: 'reset',
+    data: {},
+  });
+  
--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,10 +1,12 @@
 import * as config from './config';
+import * as app from './app';
 import * as launch from './launch';
 import * as iwa from './iwa';
 import * as kv from './kv';
 
 export default {
   config,
+  app,
   launch,
   iwa,
   kv,
