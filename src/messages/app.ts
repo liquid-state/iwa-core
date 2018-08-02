f696336812c807298fac1938ce83b839095fbfc3
Cyril Doussin <cyril@liquid-state.com>
2018-08-02 16:40:18 +1000

    Add utlity function to create app/set_authentication_status events.

--- src/messages/app.ts
+++ src/messages/app.ts
@@ -3,4 +3,11 @@ export const reset = () => ({
     eventType: 'reset',
     data: {},
   });
-  
+
+export const setAuthenticationStatus = (isAuthenticated: Boolean) => ({
+    domain: 'app',
+    eventType: 'set_authentication_status',
+    data: {
+      is_authenticated: isAuthenticated
+    },
+  });
